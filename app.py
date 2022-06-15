2336fc6f1121d6d4345ffdf7ba7570b6640b30aa
GaboGomezT <gomezgabriel09@gmail.com>
2022-06-15 00:35:53 -0500

    algoritmo de compresion básico

--- app.py
+++ app.py
@@ -2,12 +2,17 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
+import numpy as np
+from matplotlib.image import imread
+import matplotlib.pyplot as plt
+from PIL import Image
+
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -15,6 +20,10 @@ import os
 
 app = Flask(__name__)
 app.config.from_object('config')
+current_wd = os.getcwd()
+image_directory = os.path.join(current_wd, "images")
+uncompressed_directory = os.path.join(image_directory, "uncompressed")
+compressed_directory = os.path.join(image_directory, "compressed")
 #db = SQLAlchemy(app)
 
 # Automatically tear down SQLAlchemy.
@@ -60,6 +69,28 @@ def compresion():
 @app.route('/fft')
 def fft():
     return render_template('fft.html')
+
+@app.route('/fft2', methods=["POST"])
+def fft2():
+    perc = 0.1
+    file = request.files["the_file"]
+    file_name = os.path.join(uncompressed_directory, file.filename)
+    file.save(file_name)
+    A = imread(file_name)
+
+    B = np.mean(A, -1)
+    Bt = np.fft.fft2(B)
+    Btsort = np.sort(np.abs(Bt.reshape(-1)))
+    thresh = Btsort[int(np.floor((1-perc)*len(Btsort)))]
+    ind = np.abs(Bt)>thresh
+    Atlow = Bt * ind
+    Alow = np.fft.ifft2(Atlow).real
+    img = Image.fromarray(Alow)
+    img = img.convert("L")
+    file_name = os.path.join(compressed_directory, file.filename)
+    img.save(file_name)
+    return send_file(file_name, mimetype=file.content_type)
+    
 
 
 # @app.route('/login')
